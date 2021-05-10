9d95a11dd6cd8ed8cb74e5fafbfe494c9fb0c7d2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:05:34 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/LaminasModuleProvider.php
+++ src/LaminasModuleProvider.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas/laminas-config-aggregator-modulemanager for the canonical source repository
- * @copyright https://github.com/laminas/laminas-config-aggregator-modulemanager/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-config-aggregator-modulemanager/blob/master/LICENSE.md New BSD License
- */
-
 declare(strict_types=1);
 
 namespace Laminas\ConfigAggregatorModuleManager;
